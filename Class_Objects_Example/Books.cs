0346f444047c70cc7148fa3c189f05cd3f759a42
Obi Codes <75355071+ObiUche@users.noreply.github.com>
2022-01-09 22:05:04 +0000

    Uploaded Commented Code

---
+++ Class_Objects_Example/Books.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Objects_Example
+{
+    class Books
+    {
+        public string Bookname;
+        public string Author;
+        public string publishingdate;
+
+
+        public static int Books_Sold;
+
+        public const int Rental_Days = 5; 
+
+        //class memebers 
+
+
+    }
+    
+   
+}
---
+++ Class_Objects_Example/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class_Objects_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Books newBook = new Books();
+            newBook.Author = "Obinna Uche";
+            // Two different objects from the same class with different authors 
+
+
+            Books newBook1 = new Books();
+            newBook1.Author = "John Doe";
+
+
+        }
+    }
+}
